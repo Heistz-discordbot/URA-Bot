8ff8c0af592dbf0a74ccaaef01f07b11ece7cb7d
Heistz-discordbot <kavithasridhar2011@gmail.com>
2026-08-03 18:07:33 +0530

    Add Render port support

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 
+const http = require("http");
+
+http.createServer((req, res) => {
+    res.write("URA Bot is alive!");
+    res.end();
+}).listen(process.env.PORT || 3000);
+
+
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
@@ -8,37 +16,47 @@ const client = new Client({
     ]
 });
 
-client.once("ready", () => {
+
+client.once("clientReady", () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
+
 
 client.on("interactionCreate", async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === "ping") {
-        await interaction.reply("Pong 🏓");
-    }
+    try {
 
-    if (interaction.commandName === "say") {
-
-        const ownerId = "1455426573741330495";
-
-        if (interaction.user.id !== ownerId) {
-            return interaction.reply({
-                content: "❌ You cannot use this command.",
-                ephemeral: true
-            });
+        if (interaction.commandName === "ping") {
+            await interaction.reply("Pong 🏓");
         }
 
-        const text = interaction.options.getString("message");
 
-        await interaction.reply({
-            content: "✅ Message sent!",
-            ephemeral: true
-        });
+        if (interaction.commandName === "say") {
 
-        await interaction.channel.send(text);
+            const ownerId = "1455426573741330495";
+
+            if (interaction.user.id !== ownerId) {
+                return interaction.reply({
+                    content: "❌ You cannot use this command.",
+                    ephemeral: true
+                });
+            }
+
+            const message = interaction.options.getString("message");
+
+            await interaction.reply({
+                content: "✅ Sent!",
+                ephemeral: true
+            });
+
+            await interaction.channel.send(message);
+        }
+
+    } catch (error) {
+        console.error(error);
     }
 });
+
 
 client.login(process.env.TOKEN);
